782562b1b2f46d4f9e6d9c52ed2b1f8e57939134
Pascal Mainini <pascal@mainini.ch>
2013-02-12 09:46:29 +0100

    cleaned up using jshint

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 /*
-Projectname: castanea
 
 TODOs
 =====
 * unit-testing (travis?)
-* jshint/lint
 * authentication
+* minify
 
 Features
 ========
@@ -13,6 +12,9 @@ Features
 * Komplett statischer Export
 
 */
+
+/*jshint node:true, bitwise:true, curly:true, immed:true, indent:4, latedef:true, newcap:true, noarg: true, noempty:true, nonew:true, quotmark:single, regexp:true, undef:true, unused: true, trailing:true */
+/*global DOCUMENT:true, HTML:true, HEAD:true, BODY:true, META:true, TITLE:true, LINK:true, SCRIPT:true, A:true, TEXTAREA:true, DIV:true */
 
 
 /***********************************************************
@@ -22,8 +24,8 @@ Features
 'use strict';
 
 // TODO: make configurable
-var DEBUG = true; 
-var LISTENPORT = 8080; 
+var DEBUG = true;
+var LISTENPORT = 8080;
 var WIKIDATA = '/tmp/wikidata';
 var PAGEPREFIX = '/page';
 var CLIENTRESOURCES = {director: '/node_modules/director/build/director.min.js',
@@ -33,13 +35,13 @@ var CLIENTRESOURCES = {director: '/node_modules/director/build/director.min.js',
                         md_editor: '/lib/wmd-editor/Markdown.Editor.js',
                         md_styles: '/lib/wmd-editor/wmd-styles.css'};
 
-require('domo').global(); 
+require('domo').global();
 
 var http = require('http'),
-	filesystem = require('fs'),
+    filesystem = require('fs'),
     url = require('url'),
     pagedown = require('pagedown'),
-	mime = require('mime'),
+    mime = require('mime'),
     director = require('director'),
     io = require('socket.io');
 
@@ -49,70 +51,82 @@ var http = require('http'),
  **********************************************************/
 
 function send404(response) {
-        DEBUG && console.log('ERROR: 404 Not found!');
+    if (DEBUG) {
+        console.log('ERROR: 404 Not found!');
+    }
 
-        response.writeHead(404, {
-            'Content-Type': 'text/plain;charset="utf-8"'
-        });
-        response.write('404 Not found!\n');
-        response.end();
+    response.writeHead(404, {
+        'Content-Type': 'text/plain;charset="utf-8"'
+    });
+    response.write('404 Not found!\n');
+    response.end();
 }
 
 function editorScript() {
     return 'var socket = io.connect("http://localhost:8080");\n\n' +
 
-           'var edit = function() {\n' +
-           '    $("#wikieditor").append(\'' +
+            'var edit = function () {\n' +
+            'HERE\n' + // TODO
+            '    $("#wikicontent").hide();\n' +
+            '    $("#wikieditor").append(\'' +
                 DIV(
                 DIV({'class': 'wmd-panel'},
                     DIV({id: 'wmd-button-bar'}),
-                    TEXTAREA({'class': 'wmd-input', id: 'wmd-input'}),
+                    TEXTAREA({'class': 'wmd-input', id: 'wmd-input'}, '%#%MARKDOWN%#%'),
                     DIV(
-                        A({href: '#/save'}, 'Save'), ' | ', 
+                        A({href: '#/save'}, 'Save'), ' | ',
                         A({href: '#/cancel'}, 'Cancel'))
-                ), 
+                ),
                 DIV({id: 'wmd-preview', 'class': 'wmd-panel wmd-preview'})) +
-           '\');\n' +
+            '\');\n' +
 
-           '    var converter = Markdown.getSanitizingConverter()\n' +
-           '    var editor = new Markdown.Editor(converter);\n' +
-           '    editor.run();\n' +
-           '};\n\n' +
+            '    var converter = Markdown.getSanitizingConverter()\n' +
+            '    var editor = new Markdown.Editor(converter);\n' +
+            '    editor.run();\n' +
+            '};\n\n' +
 
-           'var save = function() {\n' + 
-           '    $("#wikieditor").empty();\n' + 
-           '};\n\n' + 
+            'var save = function () {\n' +
+            '    $("#wikieditor").empty();\n' +
+            '    $("#wikicontent").show();\n' +
+            '};\n\n' +
 
-           'var cancel = function() {\n' + 
-           '    socket.emit("cancel", { my: "data" });\n' +
-           '    $("#wikieditor").empty();\n' + 
-           '};\n\n' + 
+            'var cancel = function () {\n' +
+            '    socket.emit("cancel", { my: "data" });\n' +
+            '    $("#wikieditor").empty();\n' +
+            '    $("#wikicontent").show();\n' +
+            '};\n\n' +
 
-           'var routes = {\n' +
-           '    \'/edit\': edit,\n' +
-           '    \'/save\': save,\n' +
-           '    \'/cancel\': cancel\n' +
-           '};\n\n' +
+            'var routes = {\n' +
+            '    \'/edit\': edit,\n' +
+            '    \'/save\': save,\n' +
+            '    \'/cancel\': cancel\n' +
+            '};\n\n' +
 
-           'var router = Router(routes);\n' +
-           'router.init();\n';
+            'var router = Router(routes);\n' +
+            'router.init();\n';
 }
 
 function loadWikiPage(name, callback) {
     var filename = WIKIDATA + '/' + name + '.md';
 
-    DEBUG && console.log('Loading page "' + filename + '"...');
+    if (DEBUG) {
+        console.log('Loading page "' + filename + '"...');
+    }
 
-    filesystem.exists(filename, function(exists) {
-        if(!exists) {
-            DEBUG && console.log('ERROR: Page "' + name + '" does not exist!');
+    filesystem.exists(filename, function (exists) {
+        if (!exists) {
+            if (DEBUG) {
+                console.log('ERROR: Page "' + name + '" does not exist!');
+            }
             callback(true, [0, 'Page "' + name + '" does not exist!']);
             return;
         }
 
-        filesystem.readFile(filename, 'utf8', function(err, file) {
-            if(err) {
-                DEBUG && console.log('ERROR: Error occured while loading page: ' + err);
+        filesystem.readFile(filename, 'utf8', function (err, file) {
+            if (err) {
+                if (DEBUG) {
+                    console.log('ERROR: Error occured while loading page: ' + err);
+                }
                 callback(true, [1, 'Error occured while loading page!', err]);
                 return;
             }
@@ -125,18 +139,22 @@ function loadWikiPage(name, callback) {
 function getFile(prefix, path) {
     var filename = prefix + '/' + path;    // TODO workaround for not working regexp in director/route...
 
-    DEBUG && console.log('Routed into getFile("' + filename + '")');
+    if (DEBUG) {
+        console.log('Routed into getFile("' + filename + '")');
+    }
 
     var that = this;
-    filesystem.exists(filename, function(exists) {
-        if(!exists) {
+    filesystem.exists(filename, function (exists) {
+        if (!exists) {
             send404(that.res);
             return;
         }
 
-        filesystem.readFile(filename, 'binary', function(err, file) {
-            if(err) {
-                DEBUG && console.log('ERROR: Error occured reading file: ' + err);
+        filesystem.readFile(filename, 'binary', function (err, file) {
+            if (err) {
+                if (DEBUG) {
+                    console.log('ERROR: Error occured reading file: ' + err);
+                }
 
                 that.res.writeHead(500, {
                     'Content-Type': 'text/plain;charset="utf-8"'
@@ -145,7 +163,7 @@ function getFile(prefix, path) {
                 that.res.end();
                 return;
             }
- 
+
             var type = mime.lookup(filename);
             that.res.writeHead(200, {
                 'Content-Type': type
@@ -157,22 +175,24 @@ function getFile(prefix, path) {
 }
 
 function getPage() {
-    DEBUG && console.log('Routed into getPage()');
+    if (DEBUG) {
+        console.log('Routed into getPage()');
+    }
 
-    var page = url.parse(this.req.url).pathname.replace(PAGEPREFIX + '/','');
+    var page = url.parse(this.req.url).pathname.replace(PAGEPREFIX + '/', '');
     var that = this;
 
-    loadWikiPage(page, function(err, page) {
+    loadWikiPage(page, function (err, markdown) {
         var pagecontent = '';
 
-        if(err) {
-            pagecontent = '<div id="errormessage">' + page[1] + '</div>';
+        if (err) {
+            pagecontent = '<div id="errormessage">' + markdown[1] + '</div>';
         }
         else {
-            pagecontent = pagedown.getSanitizingConverter().makeHtml(page); // or: new pagedown.Converter();
+            pagecontent = pagedown.getSanitizingConverter().makeHtml(markdown); // or: new pagedown.Converter();
         }
 
-        that.res.writeHead(200, {'Content-Type': 'text/html'})
+        that.res.writeHead(200, {'Content-Type': 'text/html'});
         that.res.end(
             DOCUMENT(
                 HTML({lang: 'en'},
@@ -191,16 +211,16 @@ function getPage() {
                     BODY(
                         DIV({id: 'header'},
                             DIV({id: 'title'}, 'Wiki...'),
-                            DIV({id: 'navi'}, 
+                            DIV({id: 'navi'},
                                 A({href: '#/edit'}, 'Edit page')
                             )
                         ),
                         DIV({id: 'wikieditor'}),
-                        DIV({id: 'wikicontent'}, '%#%MARKDOWN%#%')
+                        DIV({id: 'wikicontent'}, '%#%PAGECONTENT%#%')
                     )
                 )
-            ).outerHTML.replace('%#%MARKDOWN%#%', pagecontent)
-        )
+            ).outerHTML.replace('%#%PAGECONTENT%#%', pagecontent).replace('%#%MARKDOWN%#%', markdown.replace(/\n/g, '\\n'))
+        );
     });
 }
 
@@ -222,8 +242,10 @@ var route = {
         }
     },
     '/': {
-        get: function() { 
-            DEBUG && console.log('Redirecting to /page/main...');
+        get: function () {
+            if (DEBUG) {
+                console.log('Redirecting to /page/main...');
+            }
 
             this.res.writeHead(301, {'Location': '/page/main'});
             this.res.end();
@@ -233,11 +255,13 @@ var route = {
 
 var router = new director.http.Router(route);
 
-var server = http.createServer(function(req, res) {
-    DEBUG && console.log('New request for URL ' + req.url);
+var server = http.createServer(function (req, res) {
+    if (DEBUG) {
+        console.log('New request for URL ' + req.url);
+    }
 
-    router.dispatch(req, res, function(err) {
-        if(err) {
+    router.dispatch(req, res, function (err) {
+        if (err) {
             send404(res);
         }
     });
@@ -246,9 +270,9 @@ var server = http.createServer(function(req, res) {
 var ioListener = io.listen(server);
 server.listen(LISTENPORT);
 
-ioListener.sockets.on('connection', function(socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('cancel', function(data) {
-    console.log('CANCEL: ' + data);
-  });
+ioListener.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('cancel', function (data) {
+        console.log('CANCEL: ' + data);
+    });
 });

8892393b492eabd1fb2e5d547e759e78ad9e5f37
Pascal Mainini <pascal@mainini.ch>
2013-03-15 10:08:11 +0100

    commented, added jshint

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,19 +1,56 @@
+/**
+ * @file Postinstall-script for npm-package-manager
+ * @copyright 2013 Berne University of Applied Sciences (BUAS) -- {@link http://bfh.ch}
+ * @author Pascal Mainini <pascal.mainini@bfh.ch>
+ * @version 0.1.1
+ *
+ * ! WARNING ! WARNING ! WARNING ! WARNING ! WARNING ! WARNING !
+ *
+ * THIS FILE HAS NO DEFINITIVE LICENSING INFORMATION.
+ * LICENSE IS SUBJECT OF CHANGE ANYTIME SOON - DO NOT DISTRIBUTE!
+ *
+ * ! WARNING ! WARNING ! WARNING ! WARNING ! WARNING ! WARNING !
+ *
+ * This script is run by npm after the installation. It is responsible for downloading
+ * additional dependencies which are not (or not in a form needed) available as npm-packages.
+ */
+
+/*jshint node:true, bitwise:true, curly:true, immed:true, indent:2, latedef:true, newcap:true, noarg: true, noempty:true, nonew:true, quotmark:single, undef:true, unused: true, trailing:true, white:false */
+/*global EXTERNAL_FILES:true */
+
+/***********************************************************
+ * Initialisation
+ **********************************************************/
+
+'use strict';
+
 var request = require('request'),
   fs = require('fs');
 
-var EXTERNAL_FILES = [ 
+var EXTERNAL_FILES = [
   ['lib/jquery.js','http://code.jquery.com/jquery-1.9.1.min.js'],
   ['lib/jquery.rest.js','http://raw.github.com/lyconic/jquery.rest/master/jquery.rest.js'],
   ['lib/wmd-editor/Markdown.Editor.js', 'http://raw.github.com/ujifgc/pagedown/master/Markdown.Editor.js'],
   ['lib/wmd-editor/wmd-buttons.png','http://raw.github.com/ujifgc/pagedown/master/wmd-buttons.png']
 ];
 
-console.log('rswiki postinstall script running...');
 
+/***********************************************************
+ * Function definitions
+ **********************************************************/
+
+/**
+ * This function gets called when all EXTERNAL_FILES have been fetched.
+ * Currently it only informs the user, that the postinstall-script has finished its duty.
+ */
 var _fetchDone = function _fetchDone () {
   console.log('Done fetching files!');
+  console.log('rswiki postinstall script finished!');
 };
 
+/**
+ * This function asynchronously fetches all the resources given in the EXTERNAL_FILES-array
+ */
 var _fetchFiles = function _fetchFiles() {
   console.log('Fetching files...');
 
@@ -44,6 +81,13 @@ var _fetchFiles = function _fetchFiles() {
   });
 };
 
+
+/***********************************************************
+ * Main application
+ **********************************************************/
+
+console.log('rswiki postinstall script running...');
+
 fs.exists('lib/wmd-editor', function _exists (exists) {
   if (!exists) {
     console.log('Creating directory lib/wmd-editor...');
@@ -59,4 +103,3 @@ fs.exists('lib/wmd-editor', function _exists (exists) {
     _fetchFiles();
   }
 });
-
